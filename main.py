d13296d18b41c64ddedf09415ecc9a3440f268c0
elbull-tarhur <benja.toro.l98@gmail.com>
2025-06-02 19:34:45 -0500

    Creamos el codigo para la respuesta la primera pregunta

--- main.py
+++ main.py
@@ -1,24 +1,29 @@
+#Imports 
 import pandas as pd
 from sklearn.decomposition import PCA
 from sklearn.cluster import KMeans
-import matplotlib.pyplot as plt
-import seaborn as sns
 from sklearn.feature_extraction.text import TfidfVectorizer, CountVectorizer
 
+#Carga de CSV
 data = pd.read_csv('smogon.csv')
-vector = CountVectorizer()
+
+#TFID sobre la columna texto
+vector = TfidfVectorizer()
 x = vector.fit_transform(data['texto'])
+
+#Creacion de Dataframe con los tokens ordenados
 header = (sorted(vector.vocabulary_))
 tabla = pd.DataFrame(data=x.toarray(),columns=header)
+
+#Clustering con KMeans 
 km = KMeans(n_clusters=20,random_state=98)
 cluster = km.fit_predict(tabla)
 tabla["clusters"] = cluster
-print(tabla)
-#tbf = pd.DataFrame(data=x.toarray(), columns=cabecera)
 
-#km = KMeans(n_clusters=20, random_state=98)
+#tipos de pokemon que deseamos
+types=["fire","water","grass","electric","rock","ground","flying","normal","fighting","psychic","dark","ghost","bug","dragon","ice","fairy","steel","poison"]
+print(header)
+#Crear dataframe final 
+pokemones_movelist = pd.concat([data["Pokemon"], tabla[types],tabla["clusters"]], axis=1)
 
-#lista_clusters = km.fit_predict(tbf)
-#tbf["Cluster"] = lista_clusters
-
-#tbf.to_csv("tfid_smogon.csv")
+pokemones_movelist.to_csv("tfid_smogon.csv", index=False)
