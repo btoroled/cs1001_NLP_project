d297f92758b7a5a2e217fb9cd56c720ab0e97f5f
elbull-tarhur <benja.toro.l98@gmail.com>
2025-06-03 23:15:07 -0500

    Se reviso la pregunta 2 y comenzamos a desarrollar el codigo para ver los graficos

--- pregunta_2.py
+++ pregunta_2.py
@@ -31,18 +31,44 @@ pokemones_movelist = pd.concat([data["Pokemon"], tabla[types],tabla["clusters"]]
 
 pokemones_movelist.to_csv("tfid_moveset.csv", index=False)
 
-#Copia de dataframe para calcular el promedio de TFID por cada tipo de cluste
+
+#################################
+# Graficando los resultados
+#################################
+
+#Se copia el dataframe para evitar modificar el original
 df = pokemones_movelist.copy()
-#Calcular el promedio de TFID por cada tipo de cluster
+
+#################################
+# Grafico de Heatmaps
+#################################
+
+#Calculamos los promedios de TFID por tipo de cluster
 cluster_means = df.groupby('clusters').mean(numeric_only=True)
 #Exluir la columna de clusters del dataframe
 cluster_means = cluster_means.drop(columns=['clusters'], errors='ignore')
 
-#Crear heatmap de los promedios de TFID por tipo de cluster
+#Creamos heatmap de los promedios por tipo de cluster
 plt.figure(figsize=(14, 8))
 sns.heatmap(cluster_means, annot=False, cmap="YlGnBu", linewidths=0.5)
 plt.title("Mapa de calor de movimientos por tipo de pokemon")
 plt.xlabel("Tipos de Movimiento")
 plt.ylabel("Clusters")
 plt.tight_layout
+plt.show()
+
+#################################
+# Grafico de Barras de Cantidad de movimientos por Cluster
+#################################
+
+#Creamos un DataFrame para los conteos de coicidencia por clusters
+cluster_counts = df['clusters'].value_counts().sort_index()
+
+#Creamos un gráfico de barras para visualizar la cantidad de Pokémon por cluster
+plt.figure(figsize=(10,6))
+sns.barplot(x=cluster_counts.index, y=cluster_counts.values, palette='viridis')
+plt.title("Cantidad de Pokemon por Cluster")
+plt.xlabel("Clusters")
+plt.ylabel("Cantidad de Pokemon")
+plt.tight_layout()
 plt.show()
